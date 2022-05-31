a2f3b230da8ce700cb8dba622adf5c9f26cc0a85
kevin <nguyekev@uw.edu>
2022-05-31 11:45:55 -0700

    2

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -5,7 +5,7 @@ public class UniqueWords {
 
     public UniqueWords() throws IOException {
         addUniqueWordsToLinkedList();
-        addUniqueWordsToAVL();
+        addUniqueWordsToHashTable();
     }
 
     public void addUniqueWordsToLinkedList() {
@@ -24,22 +24,22 @@ public class UniqueWords {
             words = book.getWords().next();
         }
     }
-    public void addUniqueWordsToAVL() {
-        String words = book.getWords().first();
-        MyAVLTree<String> uniqueWords = new MyAVLTree<>();
-        while (words != null) {
-            if (!uniqueWords.contains(words)) {
-                if (uniqueWords.isEmpty()) {
-                    uniqueWords.add(words);
-                } else {
-                    uniqueWords.add(words);
-                }
-            }
-            words = book.getWords().next();
-        }
-    }
-    public static void main(String[] args) throws IOException {
-        new UniqueWords();
-    }
 
+    public void addUniqueWordsToHashTable() {
+        long start, finish, difference;
+        start = System.currentTimeMillis();
+        MyHashTable<String, String> uniqueWords = new MyHashTable<>();
+        for (book.words.first(); book.words.current() != null; book.words.next()) {
+            if (uniqueWords.get(book.words.current()) == null) {
+                uniqueWords.put(book.words.current(), book.words.current());
+            }
+        }
+        finish = System.currentTimeMillis();
+        difference = finish - start;
+        System.out.println("Time taken to add unique words to hash table: " + difference + " milliseconds");
+        System.out.println("Number of unique words in hash table: " + uniqueWords.size());
+        System.out.println("Number of comparisons in hash table: " + uniqueWords.comparisons);
+        System.out.println("Max probe of hash table: " + uniqueWords.maxProbe);
+        System.out.println("Extracting the key-value pairs from the hash table:" + (difference/10) + " milliseconds");
+    }
 }
